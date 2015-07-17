fdf57157319371a173aa2d13325933c4cfabb4ad
sly <sylvia@eardish.com>
2015-07-17 13:34:27 -0700

    EQ-13 #close

--- index.js
+++ index.js
@@ -3,8 +3,7 @@
 var toTest = [ 'abba', 'ab bba', 'racecar', '0101', 'a man a plan a canal panama' ],
   panama = require( './src/panama.js' ),
   cli = require( './src/cli.js' ),
-  server = require( './server.js' ),
-  cluster = require( './cluster.js' );
+  server = require( './server.js' );
 
 if ( require.main === module && process.argv[2] !== undefined ) {
   return cli;
